e3bf4ea2e73e13fece9a896ea12c72e4cdfc2f5e
vishal shukla <vishaaxl@gmail.com>
2022-09-19 19:52:00 +0530

    build:added loader

---
+++ components/Loader.tsx
@@ -0,0 +1,22 @@
+import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+
+interface Props {}
+
+const Loader: React.FC<Props> = () => {
+  return (
+    <motion.div
+      key="loader"
+      className="loader"
+      initial={{ opacity: 1 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      <div className="blink">
+        <Image src="/images/jn-logo.jpeg" alt="logo" height={150} width={150} />
+      </div>
+    </motion.div>
+  );
+};
+
+export default Loader;
--- pages/legal-reporter.tsx
+++ pages/legal-reporter.tsx
@@ -3,12 +3,36 @@ import type { NextPage } from "next";
 interface Props {}
 
 import PageHeading from "components/page-heading/PageHeading";
+import Youtube from "components/youtube/Youtube";
 
 const LegalReporter: NextPage = () => {
   return (
     <>
       <PageHeading title="Legal Reporter" />
-      <div className="container director"></div>
+      <div className="container director">
+        <a
+          href="https://www.youtube.com/c/LegalReporter"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <h1 className="hover">Legal Reporter On Youtube</h1>
+        </a>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries.
+        </p>
+        <div className="legal-reporter-wrapper">
+          <Youtube hideHeading />
+          <Youtube hideHeading />
+          <Youtube hideHeading />
+          <Youtube hideHeading />
+          <Youtube hideHeading />
+          <Youtube hideHeading />
+        </div>
+      </div>
     </>
   );
 };
