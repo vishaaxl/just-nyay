4ada05de225596fbc13c0e783d1fec01574612cc
vishal shukla <vishaaxl@gmail.com>
2023-01-18 13:32:38 +0530

    error fix 19jan

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,9 +29,8 @@ const faqs = [
   },
   {
     id: 3,
-    question:
-      "Who are these experts for consultation? Can I choose them to talk?",
-    ans: "They are highly experienced Lawyers with hands-on knowledge and expertise on all the key fields of Law in India. They all are practicing in various High Court in Indian.<br/><br/> No You cannot choose them because they are chosen by Technology Algorithm based on their practice Area and experience; this is LegalKart’s “Smart Match” Technology ensuring you to connect with best available expert.",
+    question: "Who are these experts for consultation?",
+    ans: "They are highly experienced Lawyers with hands-on knowledge and expertise on all the key fields of Law in India. They all are practicing in various High Court in India.",
   },
   {
     id: 5,
